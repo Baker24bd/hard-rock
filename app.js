fa634523707cf12dc0a3a5e2be92f804c7e6f330
Baker24bd <mdbakerbdn@gmail.com>
2021-02-09 19:29:36 +0600

    complete site

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
 const  searchSongs = () =>{
   const searchText = document.getElementById('search-field').value;
    const url = ` https://api.lyrics.ovh/suggest/${searchText}`
+       //  load data,,,
    fetch(url)
    .then(res => res.json())
    .then(data => displaySongs(data.data))
+   .catch(error => displayError('Something Went Wrong!! Please try again latter....'));
 }
-
+// error message ,,,
+const displayError = error => {
+    const errorTag = document.getElementById('error-message');
+    errorTag.innerText = error;
+}
+//display show search result area,,, title, artist, name, song..........
 const displaySongs = songs => {
     const songContainer = document.getElementById('song-container');
-    songs.forEach(song => {
+    songContainer.innerHTML = '';
+    songs.forEach(song => {       
         const songDiv = document.createElement('div');
         songDiv.className = 'single-result row align-items-center my-3 p-3'
         songDiv.innerHTML =`
         <div class="col-md-9">
             <h3 class="lyrics-name">${song.title}</h3>
             <p class="author lead">Album by <span>${song.artist.name}</span></p>
+
+            <audio controls>
+              <source src="${song.preview}" type="audio/mpeg">
+            </audio>
        </div>
         <div class="col-md-3 text-md-right text-center">
-            <button class="btn btn-success">Get Lyrics</button>
+            <button onclick="getLyric('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
        </div>
         `;
         songContainer.appendChild(songDiv);
     })
 }
+
+const getLyric = (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+  //  load data,,,
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLyrics(data.lyrics))
+    .catch(error => displayError('Sorry! I failed to load a lyrics, Please try again latter..'));
+}
+// display show lyrics data,,,
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById('song-lyrics');
+    lyricsDiv.innerText = lyrics;
+}
+
